33d0fd9f7409a565cafabce798f5968ae6174121
Pratik-0605 <pratikshinde0605@gmail.com>
2024-11-25 12:38:28 +0530

    Added the wishlist icon, cart icon added event(action) to wishlist icon. Added parameter bloc which will take homeBloc. Added HomeState and HomeActionState in home state file, Added event in the Homeevent page.

---
+++ lib/features/home/bloc/home_state.dart
@@ -0,0 +1,24 @@
+part of 'home_bloc.dart';
+
+@immutable
+abstract class HomeState {}
+
+//action state is used when the user want to execute some action later on which will be included in HomeState only.
+abstract class HomeActionState extends HomeState {}
+
+class HomeInitial extends HomeState {}
+
+// to load the data from the api it will take some time at that time we are using loading indicator and using this class.
+class HomeLoadingState extends HomeState {}
+
+//when ui has been loaded data has been fetched and loading indicator should be removed.
+class HomeLoadedSuccessState extends HomeState {}
+
+// suppose some error occurs so we have created a error class
+class HomeErrorState extends HomeState {}
+
+// Navigate to Wishlist page after clicking on wishlist button
+class HomeNavigatetoWishlistPageActionState extends HomeActionState {}
+
+// Navigate to cart page after clicking on cart button
+class HomeNavigatetoCartPageActionState extends HomeActionState {}
--- lib/features/home/home.dart
+++ lib/features/home/home.dart
@@ -1,4 +1,6 @@
+import 'package:bloc_project_akshit/features/home/bloc/home_bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -8,8 +10,33 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  final HomeBloc homeBloc = HomeBloc();
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return BlocConsumer<HomeBloc, HomeState>(
+      bloc: homeBloc,                          // It is a parameter which is going to take homebloc
+      // listenWhen: (previous, current) {},
+      // buildWhen: (previous, current) {},
+      listener: (context, state) {},
+      builder: (context, state) {
+        return Scaffold(
+          appBar: AppBar(
+            title: Text(
+              'Pratik Grocery App',
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+            actions: [
+              IconButton(onPressed: () {
+                homeBloc.add(HomeWishlistButtonNavigateEvent());
+              }, icon: Icon(Icons.favorite)),
+              IconButton(onPressed: () {}, icon: Icon(Icons.shopping_bag_outlined)),
+            ],
+            backgroundColor: Colors.indigo[400],
+          ),
+        );
+      },
+    );
   }
 }
